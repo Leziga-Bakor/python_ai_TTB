ff60db4cd304e3e6877a1c273f709165c76aeeac
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-03 23:47:07 +0000

    Convert image to white by changing pixcel value

--- openCV-5.py
+++ openCV-5.py
@@ -2,7 +2,9 @@ import cv2
 print(cv2.__version__)
 import numpy as np
 while True:
+
     frame = np.zeros([250,250],dtype=np.uint8)
+    frame[:,:] = 255
     cv2.imshow('my window',frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
