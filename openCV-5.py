433cf675ac09567c605dc15f8ef42666db3767b3
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-03 23:51:39 +0000

    Improved code to generate red image

--- openCV-5.py
+++ openCV-5.py
@@ -3,8 +3,8 @@ print(cv2.__version__)
 import numpy as np
 while True:
 
-    frame = np.zeros([250,250],dtype=np.uint8)
-    frame[:,0:125] = 225
+    frame = np.zeros([250,250,3],dtype=np.uint8)
+    frame[:,:] = (0,0,255)
     cv2.imshow('my window',frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
