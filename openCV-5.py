573cf61b27f05b3c1c4eed48de5db133e526efb3
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-03 23:49:27 +0000

    Made image half white and half black

--- openCV-5.py
+++ openCV-5.py
@@ -4,7 +4,7 @@ import numpy as np
 while True:
 
     frame = np.zeros([250,250],dtype=np.uint8)
-    frame[:,:] = 125
+    frame[:,0:125] = 225
     cv2.imshow('my window',frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
