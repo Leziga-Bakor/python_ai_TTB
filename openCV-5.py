d64bc0f8ae00a96322fabcbdbd9331392eec5db9
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-03 23:53:34 +0000

    Split image vertically to half green and half red

--- openCV-5.py
+++ openCV-5.py
@@ -5,6 +5,7 @@ while True:
 
     frame = np.zeros([250,250,3],dtype=np.uint8)
     frame[:,:] = (0,0,255)
+    frame[:,0:125] = (0,255,0)
     cv2.imshow('my window',frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
