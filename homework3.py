d5bab7783f2c7a7291d2a425f65b48116ef5b1fe
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-04 00:20:20 +0000

    Added my solution to homework3

---
+++ homework3.py
@@ -0,0 +1,14 @@
+import cv2
+print(cv2.__version__)
+import numpy as np
+while True:
+
+    frame = np.zeros([250,250,3],dtype=np.uint8)
+    frame[:,:] = (0,0,255)
+    for i in range(0,250,50):
+        for j in range(0,250,50):
+            frame[i:i+25,j:j+25]=(0,0,0)
+            frame[i+25:i+50,j+25:j+50]=(0,0,0)
+    cv2.imshow('my window',frame)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
