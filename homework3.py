7582a93b7bf76d467197c0755f2073b6d591a50a
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-05 23:07:13 +0000

    Improved code touse variables

--- homework3.py
+++ homework3.py
@@ -7,12 +7,12 @@ numSquares=int(input('How many squares? '))
 squareSize = boardSize/numSquares
 while True:
 
-    frame = np.zeros([250,250,3],dtype=np.uint8)
+    frame = np.zeros([boardSize,boardSize,3],dtype=np.uint8)
     frame[:,:] = (0,0,255)
-    for i in range(0,250,50):
-        for j in range(0,250,50):
-            frame[i:i+25,j:j+25]=(0,0,0)
-            frame[i+25:i+50,j+25:j+50]=(0,0,0)
+    for i in range(0,boardSize,squareSize*2):
+        for j in range(0,boardSize,squareSize*2):
+            frame[i:i+squareSize,j:j+squareSize]=(0,0,0)
+            frame[i+squareSize:i+squareSize*2,j+squareSize:j+squareSize*2]=(0,0,0)
     cv2.imshow('my window',frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
