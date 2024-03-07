f3d3545286908354a329bea6730afe20eba9d276
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-07 23:40:23 +0000

    Updated code for text with variable names

--- openCV-8.py
+++ openCV-8.py
@@ -6,6 +6,9 @@ circle_colour = (0,0,255)
 circle_radius = 25
 mythick = 2
 mytext = 'JESUS is Lord'
+fheight = 2
+fthick = 2
+fcolour = (0,0,255)
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
@@ -16,7 +19,7 @@ while True:
     frame[140:220,280:360]=(255,0,0)
     cv2.rectangle(frame,(280,140),(360,220),(0,255,0),2)
     cv2.circle(frame,(int(width/2),int(height/2)),circle_radius,circle_colour,mythick)
-    cv2.putText(frame, mytext, (120,60), cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255),2)
+    cv2.putText(frame, mytext, (120,60), cv2.FONT_HERSHEY_COMPLEX,fheight,fcolour,fthick)
     cv2.imshow('my WEBcam', frame)
     cv2.moveWindow('my WEBcam',0,0)
     if cv2.waitKey(1) & 0xff ==ord('q'):
