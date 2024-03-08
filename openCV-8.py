230b3542a9dec85942462de51d02ad60fa93c62e
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-08 00:06:39 +0000

    Refactored code

--- openCV-8.py
+++ openCV-8.py
@@ -2,13 +2,24 @@ import cv2
 print(cv2.__version__)
 width=640
 height=360
+
+#Rectangle property
+upperLeft = (280,140)
+lowerRight = (360,220)
+rcolour = (0,255,0)
+rthick = 2
+
+#Circle properties
 circle_colour = (0,0,255)
 circle_radius = 25
 mythick = 2
+
+#Text property
 mytext = 'JESUS is Lord'
 fheight = 2
 fthick = 2
 fcolour = (0,0,255)
+
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
@@ -17,7 +28,7 @@ cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 while True:
     ignore,  frame = cam.read()
     frame[140:220,280:360]=(255,0,0)
-    cv2.rectangle(frame,(280,140),(360,220),(0,255,0),2)
+    cv2.rectangle(frame,upperLeft,lowerRight,rcolour,rthick)
     cv2.circle(frame,(int(width/2),int(height/2)),circle_radius,circle_colour,mythick)
     cv2.putText(frame, mytext, (120,60), cv2.FONT_HERSHEY_COMPLEX,fheight,fcolour,fthick)
     cv2.imshow('my WEBcam', frame)
