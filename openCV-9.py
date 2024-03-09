df5d2cc95c8af2b08b7f2832c833295ffd7e21a3
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-09 22:48:42 +0000

    Added region of interest and created a colour and grey window

--- openCV-9.py
+++ openCV-9.py
@@ -9,6 +9,12 @@ cam.set(cv2.CAP_PROP_FPS, 30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 while True:
     ignore,  frame = cam.read()
+    frameROI=frame[150:210,250:390]
+    frameROIGray = cv2.cvtColor(frameROI,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('my ROIgray', frameROIGray)
+    cv2.moveWindow('my ROIgray', 650,90)
+    cv2.imshow('my ROI', frameROI)
+    cv2.moveWindow('my ROI', 650,0)
     cv2.imshow('my WEBcam', frame)
     cv2.moveWindow('my WEBcam',0,0)
     if cv2.waitKey(1) & 0xff ==ord('q'):
