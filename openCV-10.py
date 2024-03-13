a2dba3b849b3128cc69007935b6c15c4caee6637
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-13 22:38:15 +0000

    Added code to detect when mouse left button is clicked and left behind

--- openCV-10.py
+++ openCV-10.py
@@ -1,5 +1,12 @@
 import cv2
 print(cv2.__version__)
+def mouseClick(event, xPos,yPos,flags,params):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        print('Mouse Event was: ',event)
+        print('at Position', xPos, yPos)
+    if event==cv2.EVENT_LBUTTONUP:
+        print('Mouse Event was: ', event)
+        print('at Position',xPos,yPos)
 width=1280
 height=720
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -7,6 +14,8 @@ cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FPS, 30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
+cv2.namedWindow('my WEBcam')
+cv2.setMouseCallback('my WEBcam', mouseClick)
 while True:
     ignore,  frame = cam.read()
     cv2.imshow('my WEBcam', frame)
