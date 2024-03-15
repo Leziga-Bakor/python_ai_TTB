909b306ab3c3607e04bee3493f3c4f276e0191f2
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-15 22:48:52 +0000

    Added second trackbar and callback function

--- openCV-13.py
+++ openCV-13.py
@@ -1,7 +1,9 @@
 import cv2
 print(cv2.__version__)
-def myCallBack(val):
-    print(val)
+def myCallBack1(val):
+    print('xPos: ',val)
+def myCallBack2(val):
+    print('yPos: ', val)
 width=1280
 height=720
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -10,9 +12,10 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FPS, 30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 cv2.namedWindow('myTrackbars')
-cv2.resizeWindow('myTrackbars',100,100)
+cv2.resizeWindow('myTrackbars',400,100)
 cv2.moveWindow('myTrackbars', width, 0)
-cv2.createTrackbar('xPos', 'myTrackbars', 0,1920, myCallBack)
+cv2.createTrackbar('xPos', 'myTrackbars', 860,1920, myCallBack1)
+cv2.createTrackbar('yPos', 'myTrackbars', 540,1080, myCallBack2)
 while True:
     ignore,  frame = cam.read()
     cv2.imshow('my WEBcam', frame)
