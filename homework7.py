1e711e2ee3995da2d7b1a1c343e06a55381de660
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-16 23:22:27 +0000

    Changed code to divition by integer

--- homework7.py
+++ homework7.py
@@ -3,14 +3,16 @@ print(cv2.__version__)
 def myCallBack1(val):
     global xPos
     print('xPos: ',val)
-    xPos = val
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, val)
+    if val == 0:
+        val = 1
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, width/val)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height/val)
     
-def myCallBack2(val):
-    global yPos
-    print('yPos: ', val)
-    yPos = val
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT,val)
+# def myCallBack2(val):
+#     global yPos
+#     print('yPos: ', val)
+#     yPos = val
+    
 # def myCallBack3(val):
 #     global myRad
 #     print('Radius: ', val)
@@ -33,8 +35,8 @@ cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 cv2.namedWindow('myTrackbars')
 cv2.resizeWindow('myTrackbars',400,150)
 cv2.moveWindow('myTrackbars', width, 0)
-cv2.createTrackbar('width', 'myTrackbars', xPos,width, myCallBack1)
-cv2.createTrackbar('height', 'myTrackbars', yPos,height, myCallBack2)
+cv2.createTrackbar('width', 'myTrackbars', 1,5, myCallBack1)
+# cv2.createTrackbar('height', 'myTrackbars', yPos,5, myCallBack2)
 # cv2.createTrackbar('radius', 'myTrackbars', myRad ,int(height/2), myCallBack3)
 # cv2.createTrackbar('thickness', 'myTrackbars', myThick ,7, myCallBack4)
 
