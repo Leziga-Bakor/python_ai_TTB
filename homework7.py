8dc635c9aa585d93ced04dc6b52edbdcf67769bd
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-16 23:51:26 +0000

    Added code to resize frame

--- homework7.py
+++ homework7.py
@@ -4,30 +4,26 @@ def myCallBack1(val):
     global xPos
     print('xPos: ',val)
     xPos = val
-    # if val == 0:
-    #     val = 1
-    # cam.set(cv2.CAP_PROP_FRAME_WIDTH, width/val)
-    # cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height/val)
+
     
 def myCallBack2(val):
     global yPos
     print('yPos: ', val)
     yPos = val
     
-# def myCallBack3(val):
-#     global myRad
-#     print('Radius: ', val)
-#     myRad = val
-# def myCallBack4(val):
-#     global myThick
-#     print('thickness: ', val)
-#     myThick = val
+def myCallBack3(val):
+    if val == 0:
+        val = 1
+    width = val
+    height = int(width*9/16)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
+
 width=1280
 height=720
 xPos = 0
 yPos = 0
-myRad = 25
-myThick = 1
+
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
@@ -38,14 +34,11 @@ cv2.resizeWindow('myTrackbars',400,150)
 cv2.moveWindow('myTrackbars', width, 0)
 cv2.createTrackbar('xPos', 'myTrackbars', 0,2000, myCallBack1)
 cv2.createTrackbar('yPos', 'myTrackbars', 0,1000, myCallBack2)
-# cv2.createTrackbar('radius', 'myTrackbars', myRad ,int(height/2), myCallBack3)
+cv2.createTrackbar('width', 'myTrackbars', width ,1920, myCallBack3)
 # cv2.createTrackbar('thickness', 'myTrackbars', myThick ,7, myCallBack4)
 
 while True:
     ignore,  frame = cam.read()
-    # if myThick == 0:
-    #     myThick = (-1)
-    # cv2.circle(frame, (xPos,yPos),myRad,(255,0,0),myThick)
     cv2.imshow('my WEBcam', frame)
     cv2.moveWindow('my WEBcam',xPos,yPos)
     if cv2.waitKey(1) & 0xff ==ord('q'):
