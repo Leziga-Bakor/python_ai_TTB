90d46688937a59344cf2e0caabb6f05fc988e5e2
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-18 23:16:16 +0000

    Added mousecallback and mouseclick function

--- openCV-15.py
+++ openCV-15.py
@@ -1,5 +1,22 @@
 import cv2
 print(cv2.__version__)
+
+evt = 0
+xVal = 0
+yVal = 0
+def mouseClick(event, xPos, yPos, flags, params):
+    global evt
+    global xVal
+    global yVal
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(event)
+        xVal = xPos
+        yVal = yPos
+        evt = event
+    if event == cv2.EVENT_RBUTTONUP:
+        evt=event
+        print(event)
+
 width=1280
 height=720
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -7,6 +24,8 @@ cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FPS, 30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
+cv2.namedWindom('my Webcam')
+cv2.setMouseCallback('my Webcam', mouseClick)
 while True:
     ignore,  frame = cam.read()
     cv2.imshow('my WEBcam', frame)
