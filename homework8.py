b4614b1659554b50436c044606b615447ef9b5da
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-21 23:57:24 +0000

    Converted color from HSV to BGR

--- homework8.py
+++ homework8.py
@@ -4,6 +4,7 @@ x=np.zeros([256,180,3],dtype=np.uint8)
 for row in range(0,256):
     for column in range(0,180):
         x[row,column]=(column,row,255)
+x = cv2.cvtColor(x,cv2.COLOR_HSV2BGR)
 while True:
     cv2.imshow('my HSV',x)
     cv2.moveWindow('my HSV', 0,0)
