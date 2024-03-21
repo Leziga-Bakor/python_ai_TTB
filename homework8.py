8b31441ba5c4552e02bb19b5772c7c5e5aa6f8d5
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-21 23:59:24 +0000

    Made the frame wider

--- homework8.py
+++ homework8.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
-x=np.zeros([256,180,3],dtype=np.uint8)
+x=np.zeros([256,720,3],dtype=np.uint8)
 for row in range(0,256):
-    for column in range(0,180):
-        x[row,column]=(column,row,255)
+    for column in range(0,720):
+        x[row,column]=(int(column/4),row,255)
 x = cv2.cvtColor(x,cv2.COLOR_HSV2BGR)
 while True:
     cv2.imshow('my HSV',x)
