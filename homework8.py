446c9e139ddb1cb49d700fd6ab09507a6f3c5d3a
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-21 23:32:13 +0000

    Corrected code for imshow

--- homework8.py
+++ homework8.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 x=np.zeros([256,180,3],dtype=np.uint8)
 while True:
-    cv2.imshow('my HSV')
+    cv2.imshow('my HSV',x)
     cv2.moveWindow('my HSV', 0,0)
     if cv2.waitKey(1) & 0xff ==ord('q'):
         break
