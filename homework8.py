d61d4b93211487819af456c85b755cd9615d4233
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-21 23:52:49 +0000

    Added code to convert window to color

--- homework8.py
+++ homework8.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 x=np.zeros([256,180,3],dtype=np.uint8)
+for row in range(0,256):
+    for column in range(0,180):
+        x[row,column]=(column,row,255)
 while True:
     cv2.imshow('my HSV',x)
     cv2.moveWindow('my HSV', 0,0)
