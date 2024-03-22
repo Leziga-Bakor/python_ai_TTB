8a0cb06cac910d22d3c5e64cbba3911dcd8c76b8
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-22 00:13:50 +0000

    Added second window to vary hue value along the row

--- homework8.py
+++ homework8.py
@@ -5,9 +5,20 @@ for row in range(0,256):
     for column in range(0,720):
         x[row,column]=(int(column/4),row,255)
 x = cv2.cvtColor(x,cv2.COLOR_HSV2BGR)
+
+y=np.zeros([256,720,3],dtype=np.uint8)
+for row in range(0,256):
+    for column in range(0,720):
+        y[row,column]=(int(column/4),255,row)
+y = cv2.cvtColor(y,cv2.COLOR_HSV2BGR)
+
 while True:
     cv2.imshow('my HSV',x)
     cv2.moveWindow('my HSV', 0,0)
+
+    cv2.imshow('my HSV2',y)
+    cv2.moveWindow('my HSV2', 0,row+40)
+
     if cv2.waitKey(1) & 0xff ==ord('q'):
         break
 cv2.destroyAllWindows()
